eff1762b1071c13339f48d4fb40fd443473095b5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-30 15:51:01 +0300

    Used the function to call out the values inside the current movie object

--- type-system/interfaces.ts
+++ type-system/interfaces.ts
@@ -1,6 +1,6 @@
 const currentMovie = {
     title: "Dune",
-    date: new Date(2021, 09, 24),
+    date: new Date(2021, 9, 24),
     rating: 8.2,
     genre: ['action', 'adventure', 'drama']
 }
@@ -16,3 +16,5 @@ const logMovie = (movie: {
     console.log(`Rating: ${movie.rating}`)
     console.log(`Genre: ${movie.genre}`)
 }
+
+logMovie(currentMovie)
