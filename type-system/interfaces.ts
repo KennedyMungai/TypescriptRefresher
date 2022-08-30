228991732fdb134e180dc1367f6acae9ecfe3ebe
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-30 15:49:37 +0300

    Defined a simple log function

---
+++ type-system/interfaces.ts
@@ -0,0 +1,18 @@
+const currentMovie = {
+    title: "Dune",
+    date: new Date(2021, 09, 24),
+    rating: 8.2,
+    genre: ['action', 'adventure', 'drama']
+}
+
+const logMovie = (movie: {
+    title: string,
+    date: Date,
+    rating: Number,
+    genre: string[]
+}):void => {
+    console.log(`Title: ${movie.title}`)
+    console.log(`Date: ${movie.date}`)
+    console.log(`Rating: ${movie.rating}`)
+    console.log(`Genre: ${movie.genre}`)
+}
