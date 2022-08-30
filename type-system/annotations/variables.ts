2ba62f8913e768e464085f6820afa511ad50b8b5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-30 12:24:21 +0300

    Rearranged the code

--- type-system/annotations/variables.ts
+++ type-system/annotations/variables.ts
@@ -1,8 +1,11 @@
 let age: number = 3
+
 let first_name:string = "Genghis Khan"
 let is_married:boolean=false
+
 let no_value:null=null
 let not_assigned:undefined=undefined
+
 let today: Date=new Date(2022, 08, 30)
 
 
