8a8eaee4bbe73ce8d374ddfc9cbc6217f37dd880
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-30 15:34:21 +0300

    Added the currenciesToDollar tuple

--- type-system/tuples.ts
+++ type-system/tuples.ts
@@ -8,3 +8,5 @@ type Track = [string, boolean, number]
 
 const track1: Track = ['rock', false, 120]
 const track2: Track = ["Hip Hop", false, 150]
+
+const currenciesToDollar:[number, number]=[1.38, 1.16]
