75dc21e63d0a8bea75b77919758d9ddac27b9ffc
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-30 15:56:50 +0300

    Added a comma to make the interface work

--- type-system/interfaces.ts
+++ type-system/interfaces.ts
@@ -2,14 +2,20 @@ interface Movie {
     title: string,
     date: Date,
     rating: number,
-    genre: string[]
+    genre: string[],
+
+    report(): string
 }
 
-const currentMovie = {
+const currentMovie: Movie = {
     title: "Dune",
     date: new Date(2021, 9, 24),
     rating: 8.2,
-    genre: ['action', 'adventure', 'drama']
+    genre: ['action', 'adventure', 'drama'],
+
+    report(): string{
+        return `Title: ${this.title}`
+    }
 }
 
 const logMovie = (movie: Movie):void => {
