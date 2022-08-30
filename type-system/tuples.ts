a0602863714433eaf6854e70e7cb4f55bae8b7de
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-30 15:37:43 +0300

    Created the currencies2Dollar for reasons

--- type-system/tuples.ts
+++ type-system/tuples.ts
@@ -10,3 +10,8 @@ const track1: Track = ['rock', false, 120]
 const track2: Track = ["Hip Hop", false, 150]
 
 const currenciesToDollar:[number, number]=[1.38, 1.16]
+
+const currencies2Dollar = {
+    sterling: 1.38, 
+    euro: 1.16
+}
