1b15e594b5474d440f00d7b54c5d9ada091b449d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-30 12:35:59 +0300

    Commented out all the other code and handles the location data in its json format

--- type-system/annotations/variables.ts
+++ type-system/annotations/variables.ts
@@ -1,33 +1,39 @@
-let age: number = 3
+// let age: number = 3
 
-let first_name:string = "Genghis Khan"
-let is_married:boolean=false
+// let first_name:string = "Genghis Khan"
+// let is_married:boolean=false
 
-let no_value:null=null
-let not_assigned:undefined=undefined
+// let no_value:null=null
+// let not_assigned:undefined=undefined
 
-let today: Date=new Date(2022, 08, 30)
-
-
-let planets:string[] = ['mercury', 'venus', 'earth', 'mars', 'jupiter', 'saturn', 'uranus', 'neptune', 'pluto']
+// let today: Date=new Date(2022, 08, 30)
 
 
-// Classes
-class Phone {
-
-}
-
-let phone1: Phone = new Phone()
+// let planets:string[] = ['mercury', 'venus', 'earth', 'mars', 'jupiter', 'saturn', 'uranus', 'neptune', 'pluto']
 
 
-// Object Literals
-let product : {name:string,price:number} = {
-    name:'pen',
-    price: 15
-}
+// // Classes
+// class Phone {
+
+// }
+
+// let phone1: Phone = new Phone()
 
 
-// Functions
-const printer: (content:string) => void = (content:string) => {
-    console.log(content)
-}
+// // Object Literals
+// let product : {name:string,price:number} = {
+//     name:'pen',
+//     price: 15
+// }
+
+
+// // Functions
+// const printer: (content:string) => void = (content:string) => {
+//     console.log(content)
+// }
+
+
+// Functions that return the 'any' type
+const place='{"lat":45, "lon":-70}'
+const newLocation=JSON.parse(place)
+console.log(newLocation)
