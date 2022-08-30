37b6cf57c26646df5003398a3790d868e8936070
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-30 13:18:20 +0300

    Created two functions, one that has a return type of void and another one that has a return type of never

--- type-system/annotations/functions.ts
+++ type-system/annotations/functions.ts
@@ -7,3 +7,13 @@ console.log(multiply(8,4))
 function sum(x:number, y:number): number {
     return x+y
 }
+
+
+const printer = (content:string): void => {
+    console.log(content)
+}
+
+
+const throwError = (content: string): never => {
+    throw new Error(content)
+}
