97fedea7711d00f3c9e3151f33d1cdc768a85ffb
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-30 14:36:42 +0300

    Created the Id property that contains health info on a client

---
+++ type-system/annotations/objects.ts
@@ -0,0 +1,12 @@
+const client = {
+    id: '1234',
+    age: 25,
+    BMI: 1.5,
+    condition: {
+        mass: 80,
+        height: 185
+    },
+    setBMI(mass: number, height: number): void {
+        this.BMI = mass/(height * height)
+    }
+}
