f2eaaf3e1cc272e45200f666cbe6d9ccbb0e6e67
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-31 14:43:11 +0300

    Added extra configuration to the webpack config ts file

--- SimpleTypescriptApp/webpack.config.js
+++ SimpleTypescriptApp/webpack.config.js
@@ -3,6 +3,16 @@ const path = require('path')
 module.exports = {
     mode: 'development',
     entry: './src/index.ts',
+    devtool: "inline-source-map",
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: "ts-loader",
+                exclude: /node-modules/,
+            },
+        ],
+    },
     output: {
         path: path.resolve(__dirname, './dist'),
         filename: 'bundle.js'
