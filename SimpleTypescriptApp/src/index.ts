783b46e2d8581149fa30b60e21fbcd8307f676e6
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-31 14:50:43 +0300

    Added an import for cargo , created a class instance and console logged it

--- SimpleTypescriptApp/src/index.ts
+++ SimpleTypescriptApp/src/index.ts
@@ -1,5 +1,9 @@
+import { Cargo } from "./Cargo"
 import { Destination } from "./Destination"
 
 
 const destination=new Destination()
 console.log(destination)
+
+const cargo=new Cargo()
+console.log(cargo)
