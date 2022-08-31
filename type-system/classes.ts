b23788c624f3a52fdf3d8d8f140e0eb7aa936502
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-31 12:07:54 +0300

    Created a new method inside the class and called it via a class instance

--- type-system/classes.ts
+++ type-system/classes.ts
@@ -2,8 +2,13 @@ class Employee {
     startShift(): void {
         console.log('Employee start shift')
     }
+
+    endShift():void {
+        console.log("The shift has ended")
+    }
 }
 
 const employee1 = new Employee();
 
 employee1.startShift()
+employee1.endShift()
