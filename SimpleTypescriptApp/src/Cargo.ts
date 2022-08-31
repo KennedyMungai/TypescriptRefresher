8e22788b54c94a531f666307c04d392c48b92deb
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-31 14:48:53 +0300

    Added a constructor to the Cargo class

--- SimpleTypescriptApp/src/Cargo.ts
+++ SimpleTypescriptApp/src/Cargo.ts
@@ -2,6 +2,14 @@ import { faker } from "@faker-js/faker";
 
 
 export class Cargo {
+    constructor() {
+        this.trackingId=faker.datatype.uuid()
+        this.location={
+            lat: +faker.address.latitude,
+            lon: +faker.address.longitude
+        }
+    }
+
     trackingId: string
     location: {
         lat: number,
