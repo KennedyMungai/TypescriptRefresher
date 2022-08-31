6e3785ac799e37fa3f2e067ed6d3c942ded2c669
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-31 13:34:58 +0300

    Added the location to the constructor

--- SimpleTypescriptApp/src/Destination.ts
+++ SimpleTypescriptApp/src/Destination.ts
@@ -3,6 +3,10 @@ import { faker } from "@faker-js/faker";
 class Destination {
     constructor() {
         this.receiver=faker.name.firstName()
+        this.location={
+            lat: faker.address.latitude()
+            lon: faker.address.longitude()
+        }
     }
     
     receiver: string
