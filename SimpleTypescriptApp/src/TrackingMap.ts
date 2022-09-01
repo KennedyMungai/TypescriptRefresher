9a939ceb747e937afa28df356a1cd4e6ef2e4307
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-01 20:25:58 +0300

    Added the instance of google.maps.Map

--- SimpleTypescriptApp/src/TrackingMap.ts
+++ SimpleTypescriptApp/src/TrackingMap.ts
@@ -1,6 +1,12 @@
 export class TrackingMap {
     constructor() {
-        this.googleMap=new google.maps.Map()
+        this.googleMap=new google.maps.Map(document.getElementById("g-map"), {
+            zoom: 1.6,
+            center: {
+                lat: 0,
+                lng: 20
+            }
+        })
     }
 
     googleMap: google.maps.Map;
