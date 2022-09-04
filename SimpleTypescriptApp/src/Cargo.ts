3071ea9a80689e7e95cbf3f24a91897c7cc27a2c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-04 10:33:54 +0300

    Implemented the Entity interface inside the Cargo class

--- SimpleTypescriptApp/src/Cargo.ts
+++ SimpleTypescriptApp/src/Cargo.ts
@@ -2,24 +2,26 @@ import { faker } from "@faker-js/faker";
 import { Entity } from './TrackingMap';
 
 
-export class Cargo {
+export class Cargo implements Entity{
     constructor() {
         this.trackingId=faker.datatype.uuid()
         this.location={
             lat: +faker.address.latitude(),
             lon: +faker.address.longitude()
         }
-    }
+    };
+
+    weight: number;
 
     trackingId: string
     location: {
         lat: number,
         lon: number
-    }
+    };
 
     popupText():string {
         return `
             <h3>Cargo Name: ${this.trackingId}</h3>
         `
-    }
+    };
 }
