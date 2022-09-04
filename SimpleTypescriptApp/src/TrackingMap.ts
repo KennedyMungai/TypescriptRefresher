aef2b9da5aa4a2526a528948ebec985a0adbfbd2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-04 09:55:23 +0300

    Added a listener that generates a popup window when clicked

--- SimpleTypescriptApp/src/TrackingMap.ts
+++ SimpleTypescriptApp/src/TrackingMap.ts
@@ -28,6 +28,14 @@ export class TrackingMap {
             }
         });
 
-        
+        marker.addListener('click', () => 
+            {
+                const popup = new google.maps.InfoWindow({
+                    content: 'Here I am',
+                });
+            }
+        );
+
+        popup.open(this.googleMap, marker);
     }
 }
