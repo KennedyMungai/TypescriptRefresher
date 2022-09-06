906d4b82977649021790e39ca18392f4b50255a7
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-06 11:51:16 +0300

    Added some comparison logic inside the compare method in the Numbers class

--- SortingData/src/Numbers.ts
+++ SortingData/src/Numbers.ts
@@ -2,6 +2,6 @@ export class Numbers {
     constructor(public number:number[]) {}
 
     compare(indexLeft:number, indexRight:number): boolean {
-
+        return this.number[indexLeft] > this.number[indexRight];
     }
 }
