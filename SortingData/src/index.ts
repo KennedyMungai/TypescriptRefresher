f27ef5da1891be9dd0b63b463a59b36602e86c43
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-06 14:10:55 +0300

    Uncommented the relevant code

--- SortingData/src/index.ts
+++ SortingData/src/index.ts
@@ -3,17 +3,17 @@ import { SortData } from "./SortData";
 import { Strings } from "./Strings";
 
 
-// const dateArray=[
-//     new Date(2010,4,21),
-//     new Date(2001,8,14),
-//     new Date(2005,12,31),
-//     new Date(2016,5,3),
-// ];
+const dateArray=[
+    new Date(2010,4,21),
+    new Date(2001,8,14),
+    new Date(2005,12,31),
+    new Date(2016,5,3),
+];
 
 
-// const years: number[] = dateArray.map((date) => {
-//     return date.getFullYear();
-// });
+const years: number[] = dateArray.map((date) => {
+    return date.getFullYear();
+});
 
 
 const numbersData = new Numbers(years);
