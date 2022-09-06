7a75ad91b1b5bc94a11623d6d07e3677255c973e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-06 12:41:13 +0300

    Did some formatting

--- SortingData/src/Strings.ts
+++ SortingData/src/Strings.ts
@@ -1,11 +1,11 @@
 export class Strings {
-    constructor(public string: string){}
+    constructor(public string: string) { }
 
     get length(): number {
         return this.string.length;
     }
 
-    compare(indexLeft:number, indexRight:number):boolean {
+    compare(indexLeft: number, indexRight: number): boolean {
         return (this.string[indexLeft].toLowerCase() > this.string[indexRight].toLowerCase());
     }
 }
