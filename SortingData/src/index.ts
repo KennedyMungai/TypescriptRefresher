539ed253431c9cf430e96dac7ef8e225dfbbd93f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-06 11:29:22 +0300

    Moved the SortData class to the SortData ts file

--- SortingData/src/index.ts
+++ SortingData/src/index.ts
@@ -1,28 +1,4 @@
-class SortData {
-    constructor(public data: number[] | string) {}
 
-    sort(): void {
-        const {length}=this.data;
-
-        for (let i = 0; i < length; i++) {
-            for (let j = 0; j < length-i-1; j++) {
-                // An array of numbers
-                if (this.data instanceof Array) {
-                    if (this.data[j] > this.data[j+i]) {
-                        const temp=this.data[j];
-                        this.data[j] = this.data[j+1];
-                        this.data[j+1]=temp;
-                    }
-                }
-
-                // String
-                if(typeof this.data === 'string'){
-                    
-                }
-            }
-        }
-    }
-}
 
 
 const dateArray=[
