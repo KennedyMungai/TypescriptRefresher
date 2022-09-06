8cece80273187e12a33b4dde3a2b3a0209a55aa4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-06 11:34:49 +0300

    Performed some code refactoring on the SortData class

--- SortingData/src/SortData.ts
+++ SortingData/src/SortData.ts
@@ -6,13 +6,10 @@ export class SortData {
 
         for (let i = 0; i < length; i++) {
             for (let j = 0; j < length-i-1; j++) {
-                // An array of numbers
-                if (this.data instanceof Array) {
-                    if (this.data[j] > this.data[j+i]) {
-                        const temp=this.data[j];
-                        this.data[j] = this.data[j+1];
-                        this.data[j+1]=temp;
-                    }
+                if (this.data[j] > this.data[j+i]) {
+                    const temp=this.data[j];
+                    this.data[j] = this.data[j+1];
+                    this.data[j+1]=temp;
                 }
 
                 // String
