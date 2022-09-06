b1207d299f9c2b7c036c7e665350779b4fe31a8f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-06 10:20:25 +0300

    The compiled result

--- SortingData/build/index.js
+++ SortingData/build/index.js
@@ -4,3 +4,9 @@ class SortData {
         this.data = data;
     }
 }
+const dateArray = [
+    new Date(2010, 4, 21),
+    new Date(2001, 8, 14),
+    new Date(2005, 12, 31),
+    new Date(2016, 5, 3),
+];
