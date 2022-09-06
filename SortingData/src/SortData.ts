c752af9aa500ee55df1ad439526931a95922d6b8
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-06 12:12:51 +0300

    Refactored the code inside the SortData ts class

--- SortingData/src/SortData.ts
+++ SortingData/src/SortData.ts
@@ -9,10 +9,8 @@ export class SortData {
 
         for (let i = 0; i < length; i++) {
             for (let j = 0; j < length-i-1; j++) {
-                if (this.data[j] > this.data[j+i]) {
-                    const temp=this.data[j];
-                    this.data[j] = this.data[j+1];
-                    this.data[j+1]=temp;
+                if (this.data.compare(j, j+1)) {
+                    this.data.swap(j, j+1);
                 }
 
                 // String
