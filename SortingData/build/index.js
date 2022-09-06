b61922d783ccab917a21f8cb966609222253d890
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-06 11:08:17 +0300

    The compiled result

--- SortingData/build/index.js
+++ SortingData/build/index.js
@@ -7,10 +7,12 @@ class SortData {
         const { length } = this.data;
         for (let i = 0; i < length; i++) {
             for (let j = 0; j < length - i - 1; j++) {
-                if (this.data[j] > this.data[j + i]) {
-                    const temp = this.data[j];
-                    this.data[j] = this.data[j + 1];
-                    this.data[j + 1] = temp;
+                if (this.data instanceof Array) {
+                    if (this.data[j] > this.data[j + i]) {
+                        const temp = this.data[j];
+                        this.data[j] = this.data[j + 1];
+                        this.data[j + 1] = temp;
+                    }
                 }
             }
         }
