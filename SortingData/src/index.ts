dd4d6c3f65be9929ba0813abc7d4e03047429564
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-06 10:38:46 +0300

    Implemented the swapping logic inside the if statement

--- SortingData/src/index.ts
+++ SortingData/src/index.ts
@@ -7,7 +7,9 @@ class SortData {
         for (let i = 0; i < length; i++) {
             for (let j = 0; j < length-i-1; j++) {
                 if (this.data[j] > this.data[j+i]) {
-                    
+                    const temp=this.data[j];
+                    this.data[j] = this.data[j+1];
+                    this.data[j+1]=temp;
                 }
             }
         }
