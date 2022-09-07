a1b6f747659cc9043f3f5c380f76bb17e49f709b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-07 11:45:22 +0300

    Added a parameter of type Props to the component

--- todolist/src/index.tsx
+++ todolist/src/index.tsx
@@ -7,8 +7,8 @@ interface Props {
 }
 
 
-const App:React.FC<Props>=()=> {
-    return <h1>{this.props.color}</h1>
+const App:React.FC<Props>=({color}: Props)=> {
+    return <h1>{color}</h1>
 }
 
-ReactDOM.render(<App color="green"/>, document.querySelector('#root'));
+ReactDOM.render(<App color="blue"/>, document.querySelector('#root'));
