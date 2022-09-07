b89d1465832d150c3574f3a1a07ef4080e2f4622
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-07 11:39:52 +0300

    Rendered out the color property in props inside tyhe root id inside the DOM

--- todolist/src/index.tsx
+++ todolist/src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const App=()=> {
-    return <h1>Snake</h1>
+    return <h1>{this.props.color}</h1>
 }
 
 ReactDOM.render(<App color="green"/>, document.querySelector('#root'));
